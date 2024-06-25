b7cb3b6e6ef09cc332c79059fc92038453931841
Suha kras <169065401+SuhaTayseer-96@users.noreply.github.com>
2024-06-25 22:25:19 +0300

    Add files via upload

---
+++ 25.06.2024/25.06.2024/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._06._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*First*/
+            //**************************************
+            //Console.WriteLine("Enter Name: ");
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("The Name is:" + Name);
+
+            /* Second*/
+            //**************************************
+            //double myDoubleNo = 7.77D;
+            //string str = "GoodBye!";
+            //char letter = 'S';
+            //bool theBool = false;
+            //int myFavNo = 7;
+            //const int myAge = 28;
+            //Console.WriteLine(myDoubleNo);
+            //Console.WriteLine(str);
+            //Console.WriteLine(letter);
+            //Console.WriteLine(theBool);
+            //Console.WriteLine(myFavNo);
+            //Console.WriteLine(myAge);
+
+            /*Third*/
+            //**************************************
+            //string [] arr1 = { "Toyota", "Ford", "BMW" };
+            //Console.WriteLine(arr1.Length);
+
+            /*Fourth*/
+            //**************************************
+            //1
+            /* string firstName = "Suha";
+            string lastName = "Kras";
+            int yearOfBirth = 1996;
+            Console.Write(firstName + " " + lastName + " " + " " + yearOfBirth);*/
+
+            //2 
+            /* string firstName = "Suha";
+            string lastName = "Kras";
+            int yearOfBirth = 1996;
+            Console.Write(firstName);
+            Console.Write(lastName);    
+            Console.WriteLine(yearOfBirth);*/
+
+            /*Fifth 
+            *****************************************/
+            //int[] numbers = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Console.WriteLine("Elements in array are:");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write($"{numbers[i]} ");
+            //}
+            //Console.WriteLine();
+
+
+            /*sixth 
+            *****************************************/
+            //int[] arrSum = { 2, 5, 8 };
+            //int sum = arrSum.Sum();
+            //Console.WriteLine(sum);
+        }
+    }
+}
